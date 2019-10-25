4befbff28c990a3f7aa3c255c329e4e4c4ee1cdb
Romuald Brillout <github.com@brillout.com>
2019-10-25 18:50:51 +0200

    wip

--- indx.js
+++ indx.js
@@ -1,1 +1,7 @@
-console.log('bla for node/browser');
+import React from 'react'
+
+const el = React.createElement('div');
+
+console.log('bla for node/browser -- dirname:'+__DIRNAME);
+
+export default el;
