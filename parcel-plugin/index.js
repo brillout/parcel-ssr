285f63d0ee3b534e58b8c8daeec79daa19f5876b
Romuald Brillout <github.com@brillout.com>
2019-10-30 12:29:18 +0100

    wip

--- parcel-plugin/index.js
+++ parcel-plugin/index.js
@@ -4,6 +4,28 @@ const {Reporter} = require('@parcel/plugin');
 
 module.exports = new Reporter({
   async report(event) {
-     process.stdout.write('\n\n'+event.type+'\n\n');
+     log(event.type);
+     if( event.type === 'buildSuccess' ){
+       log('s1');
+       try {
+       const bundles = event.bundleGraph.getBundles();
+       log(bundles.length);
+       log(Object.keys(bundles[0]).length);
+       log(bundles[0].id);
+       log(bundles[0].type);
+       log(bundles[0].filePath);
+       log(bundles[0].env.engines.browsers);
+       log(bundles[0].stats.size);
+         log('s2');
+       } catch(err) {
+         log('err');
+         log(err.stack);
+       }
+       log('s3');
+     }
   }
 });
+
+function log(msg) {
+  process.stdout.write(msg+'\n');
+}
