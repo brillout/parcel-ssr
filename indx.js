fe47a7b0467143dba04585e3ee283e4ae29efd1b
Romuald Brillout <github.com@brillout.com>
2019-10-30 12:58:17 +0100

    wip

--- indx.js
+++
@@ -1,5 +0,0 @@
-console.log('bla for node/browser -- dirname:'+__dirname);
-
-const answer = 42;
-
-export default answer;
---
+++ pages/LandingPage.js
@@ -0,0 +1,17 @@
+console.log('bla for node/browser -- dirname:'+__dirname);
+
+import React from 'react';
+
+const LandingPage = () => {
+   return (
+     <div>
+       Hello from Parcel SSR!
+     </div>
+   );
+};
+
+if( typeof window === "undefined" ){
+  window.__parcel_ssr__page_view = LandingPage;
+}
+
+export default LandingPage;
