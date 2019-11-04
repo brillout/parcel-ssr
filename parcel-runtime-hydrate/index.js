f41d9be10e45269c4545438385cc334e43e3751f
Romuald Brillout <github.com@brillout.com>
2019-11-04 15:39:50 +0100

    wip

--- parcel-runtime-hydrate/index.js
+++ parcel-runtime-hydrate/index.js
@@ -5,12 +5,25 @@ module.exports = new Runtime({
     log('aa');
     log(bundle.name);
     log(bundle.filePath);
+    const entries = bundle.getEntryAssets();
+    log(entries.length);
+    const src = bundle.getMainEntry();
+    log('ii1');
+    log(entries[0].id);
+    log(src.id);
+    log('ii2');
+    const filePath = src.filePath;
+    log(filePath);
     log('bb');
-    return {
-      filePath: __filename,
-      code: "console.log('helo frm prl rtm');",
-      isEntry: true
-    };
+    log('cc');
+    if( filePath.endsWith('LandingPage.js') ) {
+ // if( bundle.filePath.endsWith('.page.js') ) {
+      return {
+        filePath: __filename,
+        code: "const p = parcelRequire('"+src.id+"');console.log('helo frm prl rtm', p);",
+        isEntry: true
+      };
+    }
   }
 });
 
