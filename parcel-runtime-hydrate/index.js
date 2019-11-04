bab909912c705fb8f4a5b97bb0778f740d4587da
Romuald Brillout <github.com@brillout.com>
2019-11-04 16:04:39 +0100

    wip

--- parcel-runtime-hydrate/index.js
+++ parcel-runtime-hydrate/index.js
@@ -1,26 +1,16 @@
 const {Runtime} = require('@parcel/plugin');
+const path = require('path');
 
 module.exports = new Runtime({
   apply({bundle, bundleGraph, options}) {
-    log('aa');
-    log(bundle.name);
-    log(bundle.filePath);
-    const entries = bundle.getEntryAssets();
-    log(entries.length);
     const src = bundle.getMainEntry();
-    log('ii1');
-    log(entries[0].id);
-    log(src.id);
-    log('ii2');
     const filePath = src.filePath;
-    log(filePath);
-    log('bb');
-    log('cc');
+    const filePathRelative = path.relative(__dirname, filePath);
     if( filePath.endsWith('LandingPage.js') ) {
  // if( bundle.filePath.endsWith('.page.js') ) {
       return {
         filePath: __filename,
-        code: "const p = parcelRequire('"+src.id+"');console.log('helo frm prl rtm', p);",
+        code: "const page = require('"+filePathRelative+"');console.log('helo frm prl rtm', page);",
         isEntry: true
       };
     }
