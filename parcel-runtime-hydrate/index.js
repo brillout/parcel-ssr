33f7208168ea72ab8498c362dbe5fb765923e940
Romuald Brillout <github.com@brillout.com>
2019-11-04 16:01:15 +0100

    wip

--- parcel-runtime-hydrate/index.js
+++ parcel-runtime-hydrate/index.js
@@ -3,26 +3,14 @@ const path = require('path');
 
 module.exports = new Runtime({
   apply({bundle, bundleGraph, options}) {
-    log('aa');
-    log(bundle.name);
-    log(bundle.filePath);
-    const entries = bundle.getEntryAssets();
-    log(entries.length);
     const src = bundle.getMainEntry();
-    log('ii1');
-    log(entries[0].id);
-    log(src.id);
-    log('ii2');
     const filePath = src.filePath;
     const filePathRelative = path.relative(__dirname, filePath);
-    log(filePath);
-    log(filePathRelative);
-    log('bb');
-    log('cc');
     return {
       filePath: __filename,
       code: "const expValue = require('"+filePathRelative+"');console.log('helo frm prl rtm', expValue);",
-      isEntry: false
+      // What does `isEntry` mean in the context? Isn't every runtime an entry?
+      isEntry: true
     };
   }
 });
